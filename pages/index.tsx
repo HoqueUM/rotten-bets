8b0ee64076f74fab84fea15b9f77dc97e52bc562
Rahul Hoque <rhoque@umich.edu>
2024-08-29 07:23:03 -0400

    ok added beetljuice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import FormatOutput from "@/utils/FormatOutput";
+import Beetlejuice from "@/utils/Beetljuice";
 import Form from "@/components/Form";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +11,10 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <FormatOutput />
+      <div className="h-screen">
+        <FormatOutput />
+      </div>
+      <Beetlejuice />
       <div className='pt-72'>
           <a href="https://tally.so/r/3NLBOO" target="_blank">
             <button className='btn btn-neutral btn-sm'>Suggest a feature</button>
---
+++ utils/Beetljuice.tsx
@@ -0,0 +1,111 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import TimeSeriesChart from '@/components/model/Chart';
+import Form from '@/components/Form';
+async function getProps() {
+    const url = `https://rotten-bets-backend.onrender.com/current2`;
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+export default function Beetlejuice() {
+    interface timestamp {
+        time: string;
+        score: number;
+    }
+
+    type Movie = {
+        actual_count: number;
+        actual_score: number;
+        num_disliked: number;
+        num_liked: number;
+        percent_score: number;
+        title: string;
+        total_ratings: number;
+        total_reviews: number;
+        liked: number;
+        disliked: number;
+        high: number;
+        low: number;
+        timestamps: timestamp[];
+    }
+    const [data, setData] = useState<Movie>({
+        actual_count: 0,
+        actual_score: 0,
+        num_disliked: 0,
+        num_liked: 0,
+        percent_score: 0,
+        title: '',
+        total_ratings: 0,
+        total_reviews: 0,
+        high: 0,
+        low: 0,
+        liked: 0,
+        disliked: 0,
+        timestamps: [],
+    });
+    const [loading, setLoading] = useState(true);
+    const [timer, setTimer] = useState(120); 
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const data = await getProps();
+            setData(data);
+            setLoading(false);
+        };
+        fetchData();
+        const interval = setInterval(fetchData, 120000); 
+        return () => clearInterval(interval); 
+    }, []);
+
+    useEffect(() => {
+        const countdown = setInterval(() => {
+            setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 120));
+        }, 1000);
+        return () => clearInterval(countdown); 
+    }, []);
+
+    if (loading) {
+        return (
+            <div className='flex justify-center items-center h-screen'>
+                <div className='text-4xl'>Loading...</div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <div className='flex justify-center items-center flex-col p-4 mt-12 overflow-auto' style={{ maxHeight: 'calc(100vh - 5rem)' }}>
+                <div className='z-50 top-0 left-0 p-4 text-lg'>Next update in: {timer} seconds</div>
+                <p className='text-6xl font-bold'>{data.title}</p>
+                <div className='flex flex-row gap-16 p-4'>
+                    <div className='flex justify-center items-center'>
+                        <div className='text-4xl'>{data.percent_score}%</div>
+                    </div>
+                    <div className='flex flex-col justify-center items-center space-y-2'>
+                        <p className='text-2xl'>({data.actual_score})</p>
+                        <div className='text-lg'>On {data.actual_count} Reviews</div>
+                    </div>
+                </div>
+                <div className='flex flex-row gap-4'>
+                    <p className='text-xl'>{data.num_liked} Fresh {data.num_disliked} Rotten</p>                
+                </div>
+                <div className='flex flex-col gap-4 justify-center items-center'>
+                    <br />
+                    <p className='font-bold'>Relevant Kalshi Strikes:</p>
+                    <p>{data.liked} fresh(es) to get above {data.high}%</p>
+                    <p>{data.disliked} rot(s) to get to {data.low}%</p>
+                </div>
+                <br />
+                <p className='font-bold pb-4'>Score Graph:</p>
+                <div className='flex flex-col gap-4 justify-center items-center w-1'>
+                    <TimeSeriesChart data={data.timestamps} />
+                </div>
+                <br />
+            </div>
+        </div>
+    );
+}
